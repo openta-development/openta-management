3f05a74e508a25377ebe476d658d30a39c2485b2
Stellan Ostlund <stellan.ostlund@gmail.com>
2022-08-23 14:21:12 +0200

    Initial commit

---
+++ servermanager/opentasites/admin.py
@@ -0,0 +1,69 @@
+from django.contrib import admin
+from .models import OpenTASite
+
+import logging
+from django.db import models
+import uuid
+from django_json_widget.widgets import JSONEditorWidget
+from django import forms
+from django.conf import settings
+#from django.contrib.sites.models import Site
+from django.db.models import JSONField
+from django.contrib import admin
+import random
+from django.conf import settings
+
+
+# Register your models here.
+
+class OpenTASiteAdminForm(forms.ModelForm):
+
+    class Meta :
+        model = OpenTASite
+        fields = ('id','subdomain','db_name','db_label','data')
+        widgets = {
+            'data': JSONEditorWidget
+        }
+
+class OpenTASiteAdmin( admin.ModelAdmin ):
+
+    def get_readonly_fields(self, request, obj=None):
+        ro_fields = super(OpenTASiteAdmin, self).get_readonly_fields(request, obj)
+        if request.user.username == 'super' :
+            ro_fields = ()
+        return ro_fields
+
+    def get_queryset(self,request ) :
+        qs = super(OpenTASiteAdmin,self).get_queryset(request)
+        for q in qs :
+            if not 'description' in q.data.keys() :
+                q.data['description'] = ''
+                q.save()
+        return qs
+
+
+    #def get_queryset(self,request ) :
+    #   qs = super(OpenTASiteAdmin,self).get_queryset(request)
+    #   if request.user.username == 'super' :
+    #       return qs
+    #   else :
+    #       return qs.filter(subdomain=settings.SUBDOMAIN)
+
+
+
+    model = OpenTASite
+    list_display = ['id','subdomain','db_name','db_label','last_activity','creator','data']
+    readonly_fields =   ('id','subdomain','db_name','db_label','last_activity','creator')
+    form = OpenTASiteAdminForm
+
+
+
+#def getfirstOpenTASite() :
+#    logger.debug("GET FIRRST OPENTA SITE")
+#    logger.debug("SUBDOMAIN = ", settings.SUBDOMAIN)
+#    obj, created = OpenTASite.objects.get_or_create(subdomain=settings.SUBDOMAIN)
+#    return obj.id
+
+
+
+admin.site.register( OpenTASite, OpenTASiteAdmin)
---
+++ servermanager/opentasites/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+
+# Create your models here.
+
+import logging
+from django.db import models
+import uuid
+from django_json_widget.widgets import JSONEditorWidget
+from django import forms
+from django.conf import settings
+#from django.contrib.sites.models import Site
+from django.db.models import JSONField
+from django.contrib import admin
+import random
+from django.conf import settings
+
+logger = logging.getLogger(__name__)
+
+
+def random_db_name( subdomain ):
+    return "%s-%s" % ( str( subdomain ), random.randint(10000,99999) )
+
+class OpenTASite(models.Model ):
+
+    subdomain = models.CharField(max_length=4096, editable=True)
+    db_name = models.CharField(max_length=4096,  editable=True)
+    db_label= models.CharField(max_length=4096,  default='', editable=True)
+    creator = models.CharField(max_length=4096,  default='', editable=True)
+    last_activity = models.DateTimeField(auto_now=True)
+    data = JSONField(default=dict)
+
+    #objects = models.Manager()
+
+    class Meta:
+        constraints = [ models.UniqueConstraint(fields=['db_name'],name="Database must be unique" ),
+                        models.UniqueConstraint(fields=['subdomain'],name="Subdomain value must be unique" ) ]
+                        
+
+    def __str__(self):
+        return self.subdomain
+
+    def save(self, *args, **kwargs):
+        logger.debug("SAVING MODEL %s " %  self.subdomain)
+        if self.db_name is None:
+            self.db_name = self.subdomain + '-' + str( random.randint( 11111,99999) )
+        if self.db_label is None:
+            self.db_name = self.subdomain
+        super(OpenTASite, self).save(*args, **kwargs)
+
+    #def getSite(self ):
+    #    return Site.objects.get_or_create(name=self.subdomain)
+
+
+
