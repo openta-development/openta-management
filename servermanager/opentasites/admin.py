490d6e8a7c2c517f7eabf8d5cd259479004e26ea
Stellan Ostlund <stellan.ostlund@gmail.com>
2022-08-30 12:33:16 +0200

    install request_username

--- servermanager/opentasites/admin.py
+++ servermanager/opentasites/admin.py
@@ -18,22 +18,34 @@ from django.conf import settings
 
 class OpenTASiteAdminForm(forms.ModelForm):
 
+    request_username = forms.CharField()
+    
+
     class Meta :
         model = OpenTASite
-        fields = ('id','subdomain','db_name','db_label','data')
+        fields = ('id','subdomain','db_name','db_label','data','request_username')
         widgets = {
             'data': JSONEditorWidget
         }
 
 class OpenTASiteAdmin( admin.ModelAdmin ):
 
+    form = OpenTASiteAdminForm
+    model = OpenTASite
+    list_display = ['id','subdomain','db_name','db_label','last_activity','creator','data']
+    readonly_fields =   ('id','subdomain','db_name','db_label','last_activity','creator')
+    form = OpenTASiteAdminForm
+
+
     def get_readonly_fields(self, request, obj=None):
         ro_fields = super(OpenTASiteAdmin, self).get_readonly_fields(request, obj)
+        print(f"GET READONLY FIELDS")
         if request.user.username == 'super' :
             ro_fields = ()
         return ro_fields
 
     def get_queryset(self,request ) :
+        print(f"ADMIN GET QUERYSET")
         qs = super(OpenTASiteAdmin,self).get_queryset(request)
         for q in qs :
             try :
@@ -44,20 +56,13 @@ class OpenTASiteAdmin( admin.ModelAdmin ):
                 pass
         return qs
 
-
-    #def get_queryset(self,request ) :
-    #   qs = super(OpenTASiteAdmin,self).get_queryset(request)
-    #   if request.user.username == 'super' :
-    #       return qs
-    #   else :
-    #       return qs.filter(subdomain=settings.SUBDOMAIN)
-
-
-
-    model = OpenTASite
-    list_display = ['id','subdomain','db_name','db_label','last_activity','creator','data']
-    readonly_fields =   ('id','subdomain','db_name','db_label','last_activity','creator')
-    form = OpenTASiteAdminForm
+    def get_form(self, request, obj=None, **kwargs):
+        print(f" ADMIN GET FORM ")
+        form = super().get_form(request, obj, **kwargs)
+        if not 'request_username' in form.base_fields :
+            form.base_fields['request_username'] = forms.fields.CharField()
+        form.base_fields['request_username'].initial = request.user.username # THIS IS WHERE request_username is set
+        return form
 
 
 
